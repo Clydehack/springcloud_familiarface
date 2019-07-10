e6aa71400bc67af17724896cc0f8861b53bffd10
Clyde <lkwowfs@163.com>
2019-07-10 10:35:16 +0800

    初始化项目

---
+++ src/main/java/com/example/familiarface/FamiliarfaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.familiarface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamiliarfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamiliarfaceApplication.class, args);
+	}
+
+}
